f0ae06ff653d05800db98ff27142c2853844cc80
Muhammad Azeem <61928905+Azeemaj101@users.noreply.github.com>
2021-09-17 23:36:53 +0500

    Update 3.0

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Button from '@mui/material/Button';
 import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import DeleteForeverTwoToneIcon from '@mui/icons-material/DeleteForeverTwoTone';
+import Tooltip from '@mui/material/Tooltip';
 
 
 function App() {
@@ -32,12 +33,16 @@ function App() {
           {count}
         </div>
         <div className="btn">
+        <Tooltip title="ADD">
         <Button variant="contained" id="1" onClick={Fun} color="success">
           <ControlPointIcon id="11" />
         </Button>
+        </Tooltip>
+        <Tooltip title="Delete">
         <Button variant="contained" id="2" onClick={Fun} onClick={Fun} color="error">
           <DeleteForeverTwoToneIcon id="aa"/>
         </Button>
+        </Tooltip>
         </div>
       </div>
     </main>
